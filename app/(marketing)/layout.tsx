977d287dad3082737764692a8e99391650132399
senyameow <arseniyxd@gmail.com>
2023-11-22 23:46:02 +0700

    started marketing page

---
+++ app/(marketing)/layout.tsx
@@ -0,0 +1,16 @@
+const Layout = ({ children }: React.PropsWithChildren) => {
+    return (
+        <div className="h-full bg-slate-50 flex flex-col">
+
+            {/* <Navbar /> */}
+
+            <main className="pt-40 pb-20 flex-1">
+                {children}
+            </main>
+
+            {/* <Footer /> */}
+        </div>
+    )
+}
+
+export default Layout;
