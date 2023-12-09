77c10a9aaecf11e205afbfe0f40f34fd5ade9781
senyameow <arseniyxd@gmail.com>
2023-12-09 15:48:15 +0700

    sync

--- app/(dashboard)/_components/Sidebar.tsx
+++ app/(dashboard)/_components/Sidebar.tsx
@@ -45,7 +45,7 @@ const Sidebar = ({ storageKey = 'sidebar-state' }: SidebarProps) => {
 
     if (!isLoaded || !isLoadedOrg || userMemberships.isLoading) return (
         <>
-            <div>
+            <div className='space-y-2'>
                 <div className='flex items-center w-full justify-between gap-2'>
                     <Skeleton className='w-[100px] h-[16px] bg-gray-300' />
                     <Skeleton className='w-[30px] h-[30px] rounded-lg bg-gray-300' />
