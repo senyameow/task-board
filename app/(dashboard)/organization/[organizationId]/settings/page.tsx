3a2c70b1cfad773817dc902ebb58ec7989f9cf56
senyameow <arseniyxd@gmail.com>
2023-12-11 23:52:11 +0700

    org profile

--- app/(dashboard)/organization/[organizationId]/settings/page.tsx
+++ app/(dashboard)/organization/[organizationId]/settings/page.tsx
@@ -1,7 +1,22 @@
+import { OrganizationProfile } from "@clerk/nextjs";
+
 const SettingsPage = () => {
     return (
-        <div>
-            settings
+        <div className="">
+            <OrganizationProfile appearance={{
+                elements: {
+                    rootBox: {
+                        height: '100%',
+                        width: '100%',
+                        boxShadow: 'none'
+                    },
+                    card: {
+                        boxShadow: 'none',
+                        border: '1px solid #e5e5e5'
+                    }
+
+                }
+            }} />
         </div>
     )
 }
