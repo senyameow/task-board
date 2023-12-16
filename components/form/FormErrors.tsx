310fe1db306d5fa0286ee588922bb8b89de016e9
senyameow <arseniyxd@gmail.com>
2023-12-16 23:52:17 +0700

    form errors

--- components/form/FormErrors.tsx
+++ components/form/FormErrors.tsx
@@ -1,3 +1,4 @@
+import { XCircle } from 'lucide-react'
 import React from 'react'
 
 interface FormErrorsProps {
@@ -6,7 +7,15 @@ interface FormErrorsProps {
 }
 
 export const FormErrors = ({ errors, id }: FormErrorsProps) => {
+    if (!errors) return null
     return (
-        <div>FormErrors</div>
+        <div id={`${id}-error`} className='mt-2 text-rose-500 text-sm'>
+            {errors?.[id]?.map((error: string) => (
+                <div key={error} className='flex items-center font-medium border-rose-500 border rounded-sm'>
+                    <XCircle className='w-4 h-4 mr-2' />
+                    {error}
+                </div>
+            ))}
+        </div>
     )
 }
